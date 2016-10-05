a8c6216950bdcca78ed7ffd3e843faff3a6be66e
Jorge Izquierdo <jorge@izqui.me>
2016-10-05 20:10:52 +0200

    Dividends tests

--- contracts/InvestmentFund.sol
+++ contracts/InvestmentFund.sol
@@ -11,6 +11,7 @@ contract InvestmentFund is Owned, StandardToken {
     uint256 public tokenSellPrice;
 
     mapping (address => uint256) public dividends;
+    event Dividends(uint perToken);
 
     function InvestmentFund(
       string tokenName,
@@ -31,12 +32,17 @@ contract InvestmentFund is Owned, StandardToken {
       addIfNewHolder(this);
     }
 
+    function availableTokens() constant returns (uint256) {
+      return balances[this];
+    }
+
     function buyTokens() {
       uint256 tokenAmount = msg.value / tokenSellPrice;
       if (balances[this] >= tokenAmount && balances[msg.sender] + tokenAmount > balances[msg.sender])  {
         balances[msg.sender] += tokenAmount;
         balances[this] -= tokenAmount;
         Transfer(this, msg.sender, tokenAmount);
+        addIfNewHolder(msg.sender);
         if (!insuranceFund.call.value(msg.value)()) {
           throw;
         }
@@ -51,6 +57,7 @@ contract InvestmentFund is Owned, StandardToken {
         address holder = tokenHolders[i];
         dividends[holder] += balances[holder] * dividendPerToken;
       }
+      Dividends(dividendPerToken);
       return true;
     }
 
--- test/investment.js
+++ test/investment.js
@@ -16,11 +16,13 @@ contract('InvestmentFund', (accounts) => {
   it("owner should be able to mint tokens", () => {
     var investment = InvestmentFund.deployed();
     var mintAmount = 1000;
-    return investment.mintToken(mintAmount, {from:accounts[0]}).then(() => {
-      return investment.balanceOf.call(accounts[0], {from:accounts[1]})
-    }).then((balance) => {
-      assert.equal(balance.valueOf(), mintAmount, "Should own minted tokens");
-    });
+    return investment.mintToken(mintAmount, {from:accounts[0]})
+      .then(() => {
+        return investment.balanceOf.call(accounts[0], {from:accounts[1]})
+      })
+      .then((balance) => {
+        assert.equal(balance.valueOf(), mintAmount, "Should own minted tokens");
+      });
   });
 
   it("not owners cannot mint tokens", () => {
@@ -29,6 +31,39 @@ contract('InvestmentFund', (accounts) => {
       .then(function(o) { assert.fail('shouldnt have succeeded') })
       .catch(function(e) {
         assert.typeOf(e, 'Error')
-    })
+    });
+  });
+
+  it("investors should be able to buy tokens", () => {
+    var investment = InvestmentFund.deployed();
+    var buyingTokens = 3;
+    var tokenPrice = web3.toWei(buyingTokens + 0.9, 'ether');
+    return investment.buyTokens({from: accounts[2], value: tokenPrice})
+      .then(() => {
+        return investment.balanceOf.call(accounts[2], {from:accounts[1]})
+      })
+      .then((balance) => {
+        assert.equal(balance.valueOf(), buyingTokens, "Should own payed for tokens");
+      });
+  });
+
+  it("shouldn't issue tokens if not enough money is sent", () => {
+    var investment = InvestmentFund.deployed();
+    return investment.buyTokens({from: accounts[2], value: web3.toWei(0.9, 'ether')})
+      .then(function(o) { assert.fail('shouldnt have succeeded') })
+      .catch(function(e) {
+        assert.typeOf(e, 'Error')
+    });
+  });
+
+  it("should split dividends proporcionally", () => {
+    var investment = InvestmentFund.deployed();
+    return investment.sendProfitsToHolders({from: accounts[0], value: web3.toWei(10, 'ether')})
+      .then(() => {
+        return investment.dividends.call(accounts[2])
+      })
+      .then((d) => {
+        assert.isAbove(d.valueOf(), 0);
+      });
   });
 });
