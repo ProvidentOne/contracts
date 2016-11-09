2836bc8d431292a702ae13162fcc406d263ea221
Jorge Izquierdo <jorge@izqui.me>
2016-11-09 12:58:29 -0800

    Accounting periods

--- contracts/persistance/AccountingPersistance.sol
+++ contracts/persistance/AccountingPersistance.sol
@@ -21,11 +21,31 @@ contract AccountingPersistance is Managed('AccountingDB') {
   }
 
   mapping (uint256 => Transaction) public transactions;
-  uint256 public lastTransation;
+  uint256 public lastTransaction;
 
   function saveTransaction(TransactionDirection direction, uint256 amount, address from, address to, string concept) {
     transactions[lastTransation] = Transaction({direction: direction, amount: amount, from: from, to: to, concept: concept, timestamp: now});
-    lastTransation += 1;
+    lastTransaction += 1;
+  }
+
+  struct AccoutingPeriod {
+    uint256 premiums;
+    uint256 claims;
+    uint256 pastLosses;
+    bool closed;
+  }
+
+  mapping (uint256 => AccountingPeriod) public accountingPeriods;
+  uint256 public currentPeriod;
+
+  function startNewAccoutingPeriod() {
+    currentPeriod += 1;
+    var lastPeriod = accountingPeriods[currentPeriod - 1];
+    var lastProfit = int256(lastPeriod.premiums) - int256(lastPeriod.claims) - int256(pastLosses);
+    if (lastProfit < 0) {
+      accountingPeriods[currentPeriod].pastLosses = uint256(-lastProfit);
+    }
+    accountingPeriods[currentPeriod - 1].closed = true;
   }
 
   function () { throw; }
